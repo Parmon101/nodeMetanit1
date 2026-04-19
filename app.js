3fe09c67f86c59dcd531d0d7ecb19a09df54dad1
parmon101 <bfjeka101@gmail.com>
2026-04-19 17:19:19 +0300

    chore: part 4.3

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
 const express = require('express');
-const fs = require('fs');
 
 const app = express();
 
-app.use(function (request, response, next) {
-  const now = new Date();
-  const hour = now.getHours();
-  const minute = now.getMinutes();
-  const second = now.getSeconds();
-  const data = `${hour}:${minute}:${second} - ${request.method} ${request.url} ${request.get('user-agent')}`;
-  console.log(data);
-
-  fs.appendFile('serer.log', data + '/n', function (error) {
-    if (error) return console.log(error);
-    console.log('write done');
-  });
-
-  next();
-});
-
-app.get('/', function (_, response) {
-  response.send('Hello World!');
+app.use('/home/foo/bar', function (_, response) {
+  response.status(402).send('Hello World!');
 });
 
 app.listen(3000, function () {
