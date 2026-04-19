321ee9d1134a9adc849aba85ab123461cac429da
parmon101 <bfjeka101@gmail.com>
2026-04-19 18:42:40 +0300

    chore: part 4.5

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.use('/static', express.static('public'));
-
-app.use('/', function (_, response) {
-  response.send('<h1>main pa</h1>');
+app.use('/home/bar', function (_, response) {
+  response.redirect('about');
+});
+app.use('/about', function (_, response) {
+  response.send('<h1>About</h1>');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port http://localhost:3000/about');
+  console.log('Example app listening on port http://localhost:3000');
 });
