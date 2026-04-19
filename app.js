208ac9508fda4c6479f6744d511c6866fbf1df58
parmon101 <bfjeka101@gmail.com>
2026-04-19 20:07:33 +0300

    chore: part 4.11

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use('/contact', function (_, response) {
   response.render('contact.hbs', {
     title: 'My contact dock',
     emailVisible: true,
-    emails: ['ololo@ya.ru', 'sssss@ds.ds'],
+    emails: 'ololo@ya.ru',
     phone: '+1212',
   });
 });
+
 app.use('/', function (_, response) {
-  response.send('main page');
+  response.render('home.hbs');
 });
 
 app.listen(3000, function () {
