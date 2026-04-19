ddb4b107a2017811353a58541036f148edfd33ac
parmon101 <bfjeka101@gmail.com>
2026-04-19 16:55:46 +0300

    chore: part 4.2

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', function (request, response) {
-  response.send('<h1>Express from GitHub</h1>');
+app.use(function (request, response, next) {
+  const now = new Date();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  const second = now.getSeconds();
+  const data = `${hour}:${minute}:${second} - ${request.method} ${request.url} ${request.get('user-agent')}`;
+  console.log(data);
+
+  fs.appendFile('serer.log', data + '/n', function (error) {
+    if (error) return console.log(error);
+    console.log('write done');
+  });
+
+  next();
 });
-app.get('/about', function (request, response) {
-  response.send('<h1>About</h1>');
-});
-app.get('/contact', function (request, response) {
-  response.send('<h1>Contact</h1>');
+
+app.get('/', function (_, response) {
+  response.send('Hello World!');
 });
 
 app.listen(3000, function () {
