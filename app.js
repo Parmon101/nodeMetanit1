d25081b47dcf57663acc6cc1221d7a22a58f1fd4
parmon101 <bfjeka101@gmail.com>
2026-04-19 18:52:18 +0300

    chore: part 4.6

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use('/home/bar', function (_, response) {
-  response.redirect('about');
+app.get('/', function (_, response) {
+  response.send('<h1>main page</h1>');
 });
-app.use('/about', function (_, response) {
-  response.send('<h1>About</h1>');
+
+app.use('/about', function (request, response) {
+  // 1
+  //   const id = request.query.id;
+  //   const userName = request.query.name;
+  //   response.send(`<h1>about page</h1>
+  // <p>id: ${id}</p>
+  // <p>name: ${userName}</p>`);
+  // 2
+  // const users = request.query.name;
+  // let responseText = '<ul>';
+  // for (username of users) {
+  //   responseText += `<li>${username}</li>`;
+  // }
+  // responseText += '</ul>';
+  // response.send(responseText);
 });
 
 app.listen(3000, function () {
