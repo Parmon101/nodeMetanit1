c929659b2bde9f3fea109ca501aed4a681db8e11
parmon101 <bfjeka101@gmail.com>
2026-04-20 19:20:15 +0300

    chore: part 4.17 - done

--- app.js
+++ app.js
@@ -1,21 +1,72 @@
 const express = require('express');
-
 const app = express();
 
-app.set('view engine', 'pug');
+const users = [
+  {
+    id: '1',
+    name: 'John',
+    age: 30,
+  },
+];
 
-app.use('/contact', function (request, response) {
-  response.render('contact', {
-    title: 'Contact page',
-    emailVisible: true,
-    emails: ['qG2qB@example.com', 'z9g5V@example.com'],
-    phone: '123-456-7890',
-  });
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/users', async (_, response) => response.send(users));
+
+app.get('/api/users/:id', async (request, response) => {
+  const id = request.params.id;
+  const user = users.find((user) => user.id === id);
+  if (user) express.response.send(user);
+  else response.sendStatus(404);
 });
-app.use('/', function (request, response) {
-  response.send('main');
+
+app.post('/api/users', async (request, response) => {
+  if (!request.body) return response.sendStatus(400);
+
+  const userName = request.body.name;
+  const userAge = request.body.age;
+  const user = {
+    id: crypto.randomUUID(),
+    name: userName,
+    age: userAge,
+  };
+
+  users.push(user);
+  response.send(user);
+  response.sendStatus(201).send('ok');
+});
+
+app.delete('/api/users/:id', async (request, response) => {
+  const id = request.params.id;
+
+  let index = users.findIndex((user) => user.id === id);
+  if (index > -1) {
+    const user = users.splice(index, 1)[0];
+    response.send(user);
+  } else {
+    response.status(404).send('User not found');
+  }
+});
+
+app.put('/api/users', async (request, response) => {
+  if (!request.body) return response.sendStatus(400);
+
+  const id = request.body.id;
+  const userName = request.body.name;
+  const userAge = request.body.age;
+
+  const index = users.findIndex((user) => user.id === id);
+  if (index > -1) {
+    const user = users[index];
+    user.age = userAge;
+    user.name = userName;
+    response.send(user);
+  } else {
+    response.status(404).send('User not found');
+  }
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port http://localhost:3000/contact');
+  console.log('Example app listening on port http://localhost:3000');
 });
